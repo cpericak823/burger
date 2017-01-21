1dfbe73f58d8f943807cd693d3dc98314a8e7d64
Cassie <cassie.pericak@northwestern.edu>
2017-01-21 16:38:38 -0600

    initial commit with file structure and node modules

---
+++ server.js
@@ -0,0 +1,12 @@
+//require node packages
+var express = require("express");
+var exprhbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+//set the port and use express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//listen to the port
+app.listen(port);
