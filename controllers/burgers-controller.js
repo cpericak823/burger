4ef3c87f0a256833eb550c14353587820c3dba6b
Cassie <cassie.pericak@northwestern.edu>
2017-01-27 22:51:42 -0600

    pushed new burger to html

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -7,68 +7,74 @@ var app = express();
 //require the burger.js file
 var databaseQueries = require("../models/burger.js");
 
+//require the connection file
 var connection = require("../config/connect.js");
-
 
 //export the routes to be accessed later
 module.exports = function(app) {
 
     //route to get the data and return an html page
     app.get("/", function(req, res) {
-        //set userInput equal to a variable to be used as a query
-        var userInput = req.body;
-        res.render("index", { index: [{ burger_name: userInput }, { devoured: false }] });
-    });
 
-    //A query which adds a new burger based on the userInput to the database
-    app.post("/", function(req, res) {
-        console.log("got to post");
+        connection.query("Select * FROM burgers", {
 
-        //set userInput equal to a variable to be used as a query
-        var userInput = req.body;
-
-        //add the burger to the database
-        connection.query("INSERT INTO burgers SET ?", {
-            burger_name: userInput.burger_name,
-            devoured: false
-        }, function(err, res) {
+        }, function(err, result) {
             if (err) {
                 console.log(err);
-            } else {
-                console.log(res);
             }
+            res.render("index", { index: result });
         });
 
-    });
+        //A query which adds a new burger based on the userInput to the database
+        app.post("/", function(req, res) {
+            console.log("got to post");
 
-    //a query that updates the burger
-    app.put("/", function(req, res) {
+            //set userInput equal to a variable to be used as a query
+            var userInput = req.body;
 
-        //update the burger in the database
-        connection.query("UPDATE burgers SET burger_name = ?", {
+            //add the burger to the database
+            connection.query("INSERT INTO burgers SET ?", {
+                burger_name: userInput.burger_name,
+                devoured: false
+            }, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/");
+                }
+            });
+
+        });
+
+        //a query that updates the burger
+        app.put("/", function(req, res) {
+
+            //update the burger in the database
+            connection.query("UPDATE burgers SET burger_name = ?", {
+                    burger_name: userInput.burger_name,
+                    devoured: true
+                },
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.redirect("/");
+                    }
+                });
+        });
+
+        //a query that deletes the burger
+        app.delete("/", function(req, res) {
+            connection.query("DELETE FROM burgers WHERE ?", {
                 burger_name: userInput.burger_name,
                 devoured: true
-            },
-            function(err, res) {
+            }, function(err, res) {
                 if (err) {
                     console.log(err);
                 } else {
                     console.log(res);
                 }
             });
-    });
-
-    //a query that deletes the burger
-    app.delete("/", function(req, res) {
-        connection.query("DELETE FROM burgers WHERE ?", {
-            burger_name: userInput.burger_name,
-            devoured: true
-        }, function(err, res) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(res);
-            }
         });
     });
 };
--- public/assets/javascript/js.js
+++ public/assets/javascript/js.js
@@ -1,24 +1,24 @@
-$(function() {
-    $("#submit").on("click", function() {
+// $(function() {
+//     $("#submit").on("click", function() {
 
-        //set userInput equal to a variable to be used as a query
-        var userInput = $("#burger_name").val().trim();
+//         //set userInput equal to a variable to be used as a query
+//         var userInput = $("#burger_name").val().trim();
 
-        //call the create new burger function
-        createNewBurger(userInput);
-    });
-    //define the createNewBurger function that adds the burger to the database, and posts the new data to the friends array 
-    function createNewBurger(userInput) {
+//         //call the create new burger function
+//         createNewBurger(userInput);
+//     });
+//     //define the createNewBurger function that adds the burger to the database, and posts the new data to the friends array 
+//     function createNewBurger(userInput) {
 
-        //use this instead of the port since the url will change after pushing to heroku
-        var currentURL = window.location.origin;
-        console.log(userInput);
+//         //use this instead of the port since the url will change after pushing to heroku
+//         var currentURL = window.location.origin;
+//         console.log(userInput);
 
-        //post the newly added data
-        $.post(currentURL + "/", { burger_name: userInput }, function() {
+//         //post the newly added data
+//         $.post(currentURL + "/", { burger_name: userInput }, function() {
 
-            //jquery to update the when clicking submit on the the survey page and display name and image of closest match
-            $(".burger_list").append("<li>" + userInput + "</li>" + "<button>" + "Devour It" + "</button>");
-        });
-    }
-});
+//             //jquery to update the when clicking submit on the the survey page and display name and image of closest match
+//             $(".burger_list").append("<li>" + userInput + "</li>" + "<button>" + "Devour It" + "</button>");
+//         });
+//     }
+// });
